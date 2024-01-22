ceedcdaebc76f48061b2c74be72be9b16ff2550d
李俊杰 <lijunjie@lijunjiedeMacBook-Pro.local>
2024-01-22 21:01:04 +0800

    start cron.sh

--- cron.py
+++ cron.py
@@ -1,27 +1,40 @@
-import schedule
 import requests
 import time
+from datetime import datetime
 
-def job():
+# API endpoint URL
+API_ENDPOINT = "http://209.97.151.168/salesMonitor/generate_transaction_report"
 
-    current_time = time.strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Job executed at {current_time}")
+# Log file path
+LOG_FILE = "/home/nginxcaoa/comfyableAOA/media/logs/cron_api_response.log"
 
-    # Make an HTTP GET request to the specified URL
-    url = 'http://localhost:8000/salesMonitor/generate_transaction_report'
-    response = requests.get(url)
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        print("Task executed successfully")
-    else:
-        print(f"Failed to execute task. Status code: {response.status_code}")
+def make_api_request():
+    try:
+        # Make GET request with headers
+        response = requests.get(API_ENDPOINT, headers=HEADERS)
+        print(response.text)
 
-# Schedule the job to run every 2 hours
-schedule.every(2).hours.do(job)
-# schedule.every(1).minutes.do(job)
+        # Log the response to the file with a formatted timestamp
+        with open(LOG_FILE, 'a') as log_file:
+            log_file.write(
+                "API Response at {}: \n".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+            log_file.write("{}\n".format(response.text))
+            print("API request successful. Response logged to", LOG_FILE)
 
-while True:
-    schedule.run_pending()
-    time.sleep(2 * 3600)  # Sleep for the scheduled interval of 2 hours
-    # time.sleep(1 * 60)  # Sleep for the scheduled interval of 2 hours
+    except requests.exceptions.RequestException as err:
+        # Log the error to the file with a formatted timestamp
+        with open(LOG_FILE, 'a') as log_file:
+            log_file.write("Error making API request at {}. Check the endpoint or connectivity.\n".format(
+                datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+            log_file.write("Error details: {}\n".format(err))
+            print("API request failed. Error logged to", LOG_FILE)
+
+if __name__ == "__main__":
+    # Run the script every 5 minutes
+    while True:
+        make_api_request()
+        time.sleep(300)  # Sleep for 300 seconds (5 minutes)
+
---
+++ start-cron.sh
@@ -0,0 +1,1 @@
+nohup python ./cron.py &
