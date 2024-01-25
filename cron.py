28a6e5b1c9435a81e8d728c7a6f93d3f083d56c8
heidichine <zancong.zhu@tenclass.com>
2024-01-25 11:58:42 +0800

    modify LOG_FILE

--- cron.py
+++ cron.py
@@ -2,11 +2,13 @@ import requests
 import time
 from datetime import datetime
 
+from comfyableAOA.settings import BASE_DIR
+
 # API endpoint URL
 API_ENDPOINT = "http://209.97.151.168/salesMonitor/generate_transaction_report"
 
 # Log file path
-LOG_FILE = "/home/nginxcaoa/comfyableAOA/media/logs/cron_api_response.log"
+LOG_FILE = f"{BASE_DIR}/media/logs/cron_api_response.log"
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
