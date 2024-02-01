bc894f76d879de28c8dfaaa09e33afe26b5d5f13
heidichine <zancong.zhu@tenclass.com>
2024-02-01 23:17:09 +0800

    1

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,11 @@
 from salesMonitor.models import DailyProductSalesAndInventory
+from django.db import connection
 
 
 def run():
-    print(DailyProductSalesAndInventory.objects.filter())
+    # print(DailyProductSalesAndInventory.objects.filter())
+
+    cursor = connection.cursor()
+    cursor.execute("select * from django_migrations")
+    raw = cursor.fetchall()
+    print(raw)
