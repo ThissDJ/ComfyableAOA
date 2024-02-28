65a1dea49459761bc37f037e74804bb927864775
heidichine <zancong.zhu@tenclass.com>
2024-02-28 18:02:53 +0800

    广告数据

--- scripts/advertising.py
+++ scripts/advertising.py
@@ -5,6 +5,8 @@ import json
 import time
 import os
 import pytz
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from sp_api.api import FulfillmentInbound
@@ -12,7 +14,7 @@ from sp_api.base import Marketplaces
 from ad_api.base import Marketplaces as AdMarketplaces
 from ad_api.api.reports import Reports
 from comfyableAOA.settings import BASE_DIR
-from salesMonitor.models import FbaShipmentVJ, ReceivedSkuQtyVJ, Product, ShippedProductSkuQty
+from salesMonitor.models import AdPerformaceDaily, FbaShipmentVJ, ReceivedSkuQtyVJ, Product, ShippedProductSkuQty
 
 
 utc_timezone = pytz.timezone('UTC')
@@ -100,45 +102,65 @@ class FulfillmentInboundClient:
 
 class ReportsClient:
     """广告相关"""
-    def __init__(self, params: dict, start_date, end_date) -> None:
+    def __init__(self, params: dict, start_date: str, end_date: str, country: str) -> None:
         self.params = params
         self.start_date = start_date
         self.end_date = end_date
-        print(f"ReportsClient init ... start_date={start_date}, end_date={end_date}")
+        self.country = country
+        self.base_file = f"{BASE_DIR}/scripts/{self.country}_"
+        # 记录报告数据
+        self.display_data = []
+        self.poduct_data = []
+        self.brands_data = []
+        print(f"ReportsClient {country} init ... start_date={start_date}, end_date={end_date}")
     
     @property
     def client(self) -> Reports:
         return Reports(**self.params)
     
     def post_report(self, body):
-        data = """
-            {
-                "name":"SP campaigns report 2/1-2/1",
-                "startDate":"2024-02-01",
-                "endDate":"2024-02-02",
-                "configuration":{
-                    "adProduct":"SPONSORED_PRODUCTS",
-                    "groupBy":["campaign"],
-                    "columns":["cost","sales7d"],
-                    "reportTypeId":"spCampaigns",
-                    "timeUnit":"SUMMARY",
-                    "format":"GZIP_JSON"
-                }
-            }
         """
-        resp = self.client.post_report(
-            body=body
-        )
-        return resp.payload
+        {'configuration': {'adProduct': 'SPONSORED_DISPLAY', 'columns': ['cost', 'sales', 'startDate'], 'filters': None, 'format': 'GZIP_JSON', 'groupBy': ['campaign'], 'reportTypeId': 'sdCampaigns', 'timeUnit': 'SUMMARY'}, 'createdAt': '2024-02-28T03:56:24.397Z', 'endDate': '2024-02-27', 'failureReason': None, 'fileSize': None, 'generatedAt': None, 'name': 'SP display campaigns report', 'reportId': '3f058019-ec9f-4e46-b2be-1959d6214f90', 'startDate': '2024-02-21', 'status': 'PENDING', 'updatedAt': '2024-02-28T03:56:24.397Z', 'url': None, 'urlExpiresAt': None}
+        """
+        try:
+            resp = self.client.post_report(
+                body=body
+            )
+            print(f"post_report reportId={resp.payload['reportId']}")
+            return resp.payload['reportId']
+        except Exception as e:
+            print(f"post_report err {e}")
+        
+        return ""
     
-    def get_report(self, report_id: str):
-        resp = self.client.get_report(reportId=report_id)
-        print(resp.payload)
+    def get_report(self, report_id: str, retry_num=10):
+        """
+        {'configuration': {'adProduct': 'SPONSORED_DISPLAY', 'columns': ['cost', 'sales', 'startDate'], 'filters': None, 'format': 'GZIP_JSON', 'groupBy': ['campaign'], 'reportTypeId': 'sdCampaigns', 'timeUnit': 'SUMMARY'}, 'createdAt': '2024-02-28T03:56:24.397Z', 'endDate': '2024-02-27', 'failureReason': None, 'fileSize': 72, 'generatedAt': '2024-02-28T03:57:30.409Z', 'name': 'SP display campaigns report', 'reportId': '3f058019-ec9f-4e46-b2be-1959d6214f90', 'startDate': '2024-02-21', 'status': 'COMPLETED', 'updatedAt': '2024-02-28T03:57:30.409Z', 'url': 'https://offline-report-storage-us-east-1-prod.s3.amazonaws.com/3f058019-ec9f-4e46-b2be-1959d6214f90-1709092589834/report-3f058019-ec9f-4e46-b2be-1959d6214f90-1709092589834.json.gz?X-Amz-Security-Token=IQoJb3JpZ2luX2VjEPz%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJHMEUCIQDTiVo0Q%2FzVvASPCtI7lpdjN4VqmAnxDsJVxNtVxvAh4wIgQO61G2KI4%2FanBE0mQFHbs8okalLDEeRP5z3z37Lh7Jsq6gUI5P%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARACGgw4NDE2NDU1NDMwNzQiDF9XksyaMO4pqVprXCq%2BBZ9%2FZAJWxDMx5CeqD9rpYHCJpouUqJTnhPPmm0nhiJzxAa6545pzHQWFV0Zq6hzxzlwwnvbYjbLXhOuQq%2BUC%2FiN8CjiF0GXJhH3pu24FVM8czCbxFQ4%2BnyUHxvutNwXDEvTPaFF0jchNV5DPaY7sLBNkI84OnUlTWGMGONplOo5xg2C8QIOcf1%2F5uLg%2FrNoO7b0qtO5SxkNDcXCxG9egLAx9LXoNHII%2BKoOiPYh5Xd02XsY1fLcle1XO3izwCBm7ubke6kOMvCBN3e5kHGd48nFuXFL3TQGeZ9rukSo0RJs82Y%2F4dh%2BWN2Y64nJiU0fd%2FDOK2ab6NfW2JXc7QDnQvX5Pq8s5%2FiPUBljxid2otN3Dci14DfudDPrsTQqpjY%2Fki3dQLlIPYYMl0w9OnmniOSganWBhrBG8d73JE%2BflQOO8f9bkuOqitVUAfsA9jPpaG45w%2FsY3Q%2F9Q5Hmyagfk9kCbmVbJdTK8s%2BGTrD8YWJv6qLevqPVYmCuHKQndTZ0HRF6U7Kv4axTYQ55CcaNt%2BvE0ZJsdvXfLvqutpPyuh6HsP9dm2XVsKxd1o93tnF%2F%2FkHaEKhwSxeaICInUNx58QN7ee2%2Fm2mMBwo0JRg0utFYZNSxVT0cePdpvJzg%2Bf5NYzSBk7WsO0CmSRZW6lfg%2B8utwcgc%2BdQMsGRY1pA7pJHubbuxXc891d6QXvuYSO7RqRNOVzIEcwEGRsRWxKgsa3W82er%2BdHJlNBcTjvJJNa7ddx0gEZXVE94IrNaSc6vGCr8bEf%2BHuNlf1m8wvodRAhcs%2Fs8%2FQEgNrHFlbndFKCrhgVa%2FTlyVDJPARbzJqSI%2BM5NamBAyBBWy1cVFrN1ZUoln1UuVp6XlZKOvc7W9kJ8tu6shdkOhQDb6B8CWUvv1NQyvVKlUn9Z%2FK5pegm52I%2B63rlIdDV3jCcZQem40YATCdzfquBjqpAbYnsmw8VTO3k2S5b2f%2Fb0o8gH7ld1Hg8a548JigpRCMl%2FAOIeCsqmF2wHfVDCazBF4lM%2BTOf2Mg3IjrmdU9P5W4MZx2DfS%2Bk2nEK2I2AqrBnMajLenvvWP6tTFGCBDnmBqzIaabwyEIEd2XN0uT7F8bju5YeF%2BP0PnJaebiJW8maZ2UHk9p%2Boa7i7kUwSHwNgW4Dm7BxtT4xDOOUpODKPlc8LKTuQZyQao%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240228T035731Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3599&X-Amz-Credential=ASIA4H5P3Z2REL37ZC55%2F20240228%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=0816d0140427aebc15c1929a666085ed075a678d1a77a9cfa94c27af72fa1f2e', 'urlExpiresAt': '2024-02-28T04:57:31.364988Z'}
+        """
+        try:
+            resp = self.client.get_report(reportId=report_id)
+            if not resp.payload['url'] and retry_num > 0:
+                retry_num -= 1
+                print(f"get_report retry {resp.payload}")
+                time.sleep(60)
+                self.get_report(report_id=report_id, retry_num=retry_num)
+            print(f"get_report [{resp.payload['url']}]")
+            return resp.payload['url']
+        except Exception as e:
+            print(f"get_report err {e}")
+        return ''
 
-    def download_report(self, url):
-        self.client.download_report(url=url, file=f"{BASE_DIR}/scripts/ad_report", format='json')
+    def download_report(self, url, filename):
+        try:
+            self.client.download_report(
+                url=url,
+                file=f"{self.base_file}{filename}_report",
+                format='json'
+            )
+        except Exception as e:
+            print(f"download_report err {e}")
 
-    def get_body(self, name, ad_product):
+    def get_body(self, name, ad_product, report_type_id, columns=None):
         body = {
             "name": name,
             "startDate": self.start_date,
@@ -146,20 +168,121 @@ class ReportsClient:
             "configuration": {
                 "adProduct": ad_product,
                 "groupBy": ["campaign"],
-                "columns": ["cost", "sales", "startDate"],
-                "reportTypeId": "sdCampaigns",
-                "timeUnit": "SUMMARY",
+                "columns": columns or ["cost", "sales", "date"],
+                "reportTypeId": report_type_id,
+                "timeUnit": "DAILY",
                 "format": "GZIP_JSON"
             }
         }
         return json.dumps(body)
+    
+    def get_name(self, prefix):
+        start_date = self.start_date.replace('-', '/')[5:]
+        end_date = self.end_date.replace('-', '/')[5:]
+        return f"{prefix} {start_date}-{end_date}"
+    
+    def get_display_report(self):
+        name = self.get_name(prefix="SD campaigns report")
+        display_body = self.get_body(
+            name=name,
+            ad_product="SPONSORED_DISPLAY",
+            report_type_id="sdCampaigns"
+        )
+        report_id = self.post_report(body=display_body)
+        if not report_id:
+            print(f"{name} not report_id")
+            return
+        
+        time.sleep(60)
+
+        url = self.get_report(report_id=report_id)
+        if not url:
+            print(f"{name} not url")
+            return
+        
+        time.sleep(5)
+        self.download_report(url=url, filename="display")
+
+        if not Path(f"{self.base_file}display_report.json").exists():
+            print(f"{name} not display_report.json")
+            return
+        
+        with open(f"{self.base_file}display_report.json", 'r', encoding='utf-8') as file:
+            self.display_data = json.load(file)
+        return
+    
+    def get_product_report(self):
+        name = self.get_name(prefix="SP campaigns report")
+        product_body = self.get_body(
+            name=name,
+            ad_product="SPONSORED_PRODUCTS",
+            report_type_id="spCampaigns",
+            columns=["cost", "sales1d", "date"]
+        )
+        report_id = self.post_report(body=product_body)
+        if not report_id:
+            print(f"{name} not report_id")
+            return
+        
+        time.sleep(60)
+
+        url = self.get_report(report_id=report_id)
+        if not url:
+            print(f"{name} not url")
+            return
+        
+        time.sleep(5)
+        self.download_report(url=url, filename="product")
+
+        if not Path(f"{self.base_file}product_report.json").exists():
+            print(f"{name} not product_report.json")
+            return
+        
+        with open(f"{self.base_file}product_report.json", 'r', encoding='utf-8') as file:
+            self.product_data = json.load(file)
+        return
+    
+    def get_brands_report(self):
+        name = self.get_name(prefix="SB campaigns report")
+        brands_body = self.get_body(
+            name=name,
+            ad_product="SPONSORED_BRANDS",
+            report_type_id="sbCampaigns"
+        )
+        report_id = self.post_report(body=brands_body)
+        if not report_id:
+            print(f"{name} not report_id")
+            return
+        
+        time.sleep(60)
+
+        url = self.get_report(report_id=report_id)
+        if not url:
+            print(f"{name} not url")
+            return
+        
+        time.sleep(5)
+        self.download_report(url=url, filename="brands")
+
+        if not Path(f"{self.base_file}brands_report.json").exists():
+            print(f"{name} not brands_report.json")
+            return
+        
+        with open(f"{self.base_file}brands_report.json", 'r', encoding='utf-8') as file:
+            self.brands_data = json.load(file)
+        
+        return
 
     def get_report_detail(self):
         """
         需要生成SPONSORED_PRODUCTS、SPONSORED_BRANDS、SPONSORED_DISPLAY三种报告
         """
-        body = self.get_body(name="SP display campaigns report", ad_product="SPONSORED_DISPLAY")
-        print(self.post_report(body=body))
+        self.get_display_report()
+        self.get_product_report()
+        self.get_brands_report()
+        self.display_data.extend(self.product_data)
+        self.display_data.extend(self.brands_data)
+        return self.display_data
 
 
 def update_shipment(params: dict, country: str):
@@ -210,19 +333,67 @@ def update_shipment(params: dict, country: str):
         )
         fba_shipment.shipped_product_sku_qties.set(shipped_product_sku_qties)
         fba_shipment.save()
+    print(f"[{country}] update_shipment finish!")
+
+
+def update_ad_performace_daily(params: dict, start_date, end_date, country):
+    client = ReportsClient(
+        params=params,
+        start_date=start_date,
+        end_date=end_date,
+        country=country
+    )
+    ad_list = client.get_report_detail()
+    aggregated_data = {}
+    for ad in ad_list:
+        ad_sales = ad.get('sales') or ad.get('sales1d') or 0
+        cost = ad.get('cost') or 0
+        date = ad['date']
+        if date in aggregated_data:
+            aggregated_data[date]["ad_sales"] += ad_sales
+            aggregated_data[date]["cost"] += cost
+        else:
+            aggregated_data[date] = {"ad_sales": ad_sales, "cost": cost}
+    
+    for date, data in aggregated_data.items():
+        AdPerformaceDaily.objects.update_or_create(
+            defaults=data,
+            date=date,
+            country=country,
+        )
+    print(f"[{country}] {aggregated_data} update finish!")
+
+
+def async_update_shipment(params: dict, country: str):
+    while True:
+        try:
+            update_shipment(params=params, country=country)
+        except Exception as e:
+            print(f"async_update_shipment {country} err {e}")
+        finally:
+            print(f"async_update_shipment {country} sleep...")
+            time.sleep(30 * 60)
+
+
+def async_update_ad_performace_daily(params: dict, country):
+    while True:
+        try:
+            end_date = get_now_date(la_timezone)
+            start_date = end_date - timedelta(days=6)
+            
+            start_date = start_date.strftime('%Y-%m-%d')
+            end_date = end_date.strftime('%Y-%m-%d')
+            update_ad_performace_daily(params=params, start_date=start_date, end_date=end_date, country=country)
+        except Exception as e:
+            print(f"async_update_ad_performace_daily {country} err {e}")
+        finally:
+            print(f"async_update_ad_performace_daily {country} sleep...")
+            time.sleep(60 * 60)
 
 
 def run():
-    # update_shipment(params=dict(credentials=au_credentials, marketplace=Marketplaces.AU), country="AU")
-    # update_shipment(params=dict(credentials=us_credentials, marketplace=Marketplaces.US), country="US")
-    start_date = get_now_date(la_timezone) - timedelta(days=6)
-    end_date = get_now_date(la_timezone)
-    client = ReportsClient(
-        params=dict(credentials=us_ad_credentials, marketplace=AdMarketplaces.US),
-        start_date=start_date.strftime('%Y-%m-%d'),
-        end_date=end_date.strftime('%Y-%m-%d'),
-    )
-    client.get_report_detail()
-    # client.post_report()
-    # client.get_report(report_id="5441528b-f78c-449b-96b1-1956ef8a5466")
-    # client.download_report(url='https://offline-report-storage-us-east-1-prod.s3.amazonaws.com/5441528b-f78c-449b-96b1-1956ef8a5466-1708931157903/report-5441528b-f78c-449b-96b1-1956ef8a5466-1708931157903.json.gz?X-Amz-Security-Token=IQoJb3JpZ2luX2VjENb%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJGMEQCIGn4pDhSuE3hhoONYWTVnE7umQPgLYOWdaNVSP8gQzPoAiA9TGM1fbUUdbrJrp8M2OMkg9WqjVlfF8shzNzdToxe4CrqBQi%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAIaDDg0MTY0NTU0MzA3NCIMXOzAxo2US2gZlPvpKr4FJGKcujWSxPP3a6GMZJyCMlIfMWtphM7ahi1%2FTx6NVRqzpEe4FcIN0rkDKQ0i476ky6P20%2B0xX7LLNTaoqcYquim8fApMH07YbOZ8H7175KF6UMc592KMSenCucEMFspyjWwoK36qD0SAsot9LV1Ex8PTbIoib9wgkBV3w2d7XuO9nbWLrZBD%2FZF6%2FDlYUej6T4loYnm7r5ztHM3tnFyfXqQd3Cmq4Pm5n8Cfe4ZrOqjkaITU6kKEvjbddALue7mQ9iUPynIX58XcIOn2rIX29IdkelHj%2FgszeThscqtw1EUGsomvvpl0cN0ty1j%2BmgX4y%2FFkYNAT%2BXrKiYLLG7DuRq1duwRC0cZa5g8FrJiMVnwjv70G5PxMxEEu4%2BE32Yd3L%2BeAODR%2Fp%2Bq7k3loDrE7oNL0RGxjqC3r%2Ba9wB%2F8fKrKYIeVMvauwnIzaRNu%2FsSvbfNWEX%2B9LIxd%2BVJcB7Ndomoi9m3PMzXrx3VF8sUJxffodLpAoddwyiXsrYnE4DbDDNWCH7J7Zj0uMbnLfp6rcxxrZpqUlFfaL62wP5CuHN%2BkIzFZ4hZjreDT8diWsfvGzFHn1h0yjPpyz5JMxZIHbfecosjDXGYApjJD%2F2zf2r4iqpa3ClcaHMB%2Buh2sq3IshZ1dyFbdcENQvQwA1tNs1tZqTdS44WgYfGnelMxUeO3EGarzzcZ5ivIS9Qx5XffxBO%2FF%2BeY%2Fn44CzFfKgbyPv9bg3HugFMmFriZmD4YXXQvRDNqiY3Usr%2F2guZhAxESQa10%2F4Mz8tOVT%2B5sQ%2BooZZEjZh%2FV8inEMqkWc16BqVT%2B2u41bxrAykyA3XDFRg4yM1wbDLNBQfdyOqL%2FIynYaYIXjdXNaxOJcldSEPx3SFni8Kx2Y7s0d40187uhTYkqgGkchx4PG0mFu2QRm3nywds3dg5XeR79TJqAnud4uPMPqg8q4GOqoBCH0WUZKA1fp8K0PE9q4jCrS7m0Ovtpb9SNuhtN4KHjNxylmX0dXF2gdGazQdK2YykQYncEgreB87W9dA4RaEzZGcJLNcj3i1S%2FUzeciR6v%2B6U%2BkJZ1INe%2BAlfyxvIdcVIopepLhMADFm0PYNdCzL0z4IJ3IJu%2FKIOI3E%2Bu2xsdL%2FW6QNqQKN9zBYwEcoFzNZU%2F5AxhO%2BAaoEan8Sghcz3MW7LF2SvWv4tVY%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240226T134455Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=ASIA4H5P3Z2RBQB6SZG4%2F20240226%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=5eb0f0c55294b00bbffd025e2cbe3e0e9da1bc79a80826b91fabf823d023da49')
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        executor.submit(async_update_shipment, dict(credentials=au_credentials, marketplace=Marketplaces.AU), "AU")
+        executor.submit(async_update_shipment, dict(credentials=us_credentials, marketplace=Marketplaces.US), "US")
+        executor.submit(async_update_ad_performace_daily, dict(credentials=us_ad_credentials, marketplace=AdMarketplaces.US), "US")
+        executor.submit(async_update_ad_performace_daily, dict(credentials=au_ad_credentials, marketplace=AdMarketplaces.AU), "AU")
